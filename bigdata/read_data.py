97ad826d9cc4154b89d10eb797ca9f55812559d2
pravinhada <pravin_hada@yahoo.com>
2013-09-24 11:11:25 -0400

    adding data files

--- bigdata/read_data.py
+++ bigdata/read_data.py
@@ -1,14 +1,16 @@
+#!/usr/bin/env python
 """
-for more information please see: http://techely.blogspot.com/2012/10/mapreduce-processing-large-data-sets.html
-"""
+Program to compute how many times every term occurs accross titles, for each author
 
+"""
 import mincemeat
 import glob
-import stopwords
+import pprint
 
 # Accessing all the files in directory
-# Set the file path here.
-file_dir = glob.glob('*')
+file_dir = glob.glob('/hw3data/*')
+
+
 
 # Read the file contain
 def contain(fname):
@@ -29,7 +31,7 @@ source_data = dict((fname, contain(fname)) for fname in file_dir)
 # mapper function, key is document id and v is contains of document
 def mapfn(k, v):
     for value in v.splitlines():
-        paper, authors, title = value.split(':::')
+        paper,authors,title = value.split(':::')
         for author in authors.split('::'):
             w = {}
             for word in title.split():
@@ -44,12 +46,27 @@ def mapfn(k, v):
 # reduce function
 def reducefn(k, vs):
     result = {}
+    # All the stopwords for filtering
+    skeys = ['all', 'herself', 'should', 'to', 'only', 'under', 'do', 'weve', 'very', 'cannot', 'werent', 'yourselves', 'him', 'did', 'these', 
+             'she', 'havent', 'where', 'whens', 'up', 'are', 'further', 'what', 'heres', 'above', 'between', 'youll', 'we', 'here', 'hers', 
+             'both', 'my', 'ill', 'against', 'arent', 'thats', 'from', 'would', 'been', 'whos', 'whom', 'themselves', 'until', 'more', 'an', 
+             'those', 'me', 'myself', 'theyve', 'this', 'while', 'theirs', 'didnt', 'theres', 'ive', 'is', 'it', 'cant', 'itself', 'im', 'in', 
+             'id', 'if', 'same', 'how', 'shouldnt', 'after', 'such', 'wheres', 'hows', 'off', 'i', 'youre', 'well', 'so', 'the', 'yours', 'being', 
+             'over', 'isnt', 'through', 'during', 'hell', 'its', 'before', 'wed', 'had', 'lets', 'has', 'ought', 'then', 'them', 'they', 'not', 
+             'nor', 'wont', 'theyre', 'each', 'shed', 'because', 'doing', 'some', 'shes', 'our', 'ourselves', 'out', 'for', 'does', 'be', 'by', 
+             'on', 'about', 'wouldnt', 'of', 'could', 'youve', 'or', 'own', 'whats', 'dont', 'into', 'youd', 'yourself', 'down', 'doesnt', 'theyd', 
+             'couldnt', 'your', 'her', 'hes', 'there', 'hed', 'their', 'too', 'was', 'himself', 'that', 'but', 'hadnt', 'shant', 'with', 'than', 
+             'he', 'whys', 'below', 'were', 'and', 'his', 'wasnt', 'am', 'few', 'mustnt', 'as', 'shell', 'at', 'have', 'any', 'again', 'hasnt', 
+             'theyll', 'no', 'when', 'other', 'which', 'you', 'who', 'most', 'ours ', 'why', 'having', 'once']
+
+    # loop starts here.
     for v in vs:
         for word in v:
-            if word in result:
-                result[word] = result[word] + 1
-            else:
-                result[word] =v[word]
+            if len(word)>1 and word.lower() not in skeys:
+                if word in result:
+                    result[word] = result[word] + 1
+                else:
+                    result[word] = v[word]
 
     return result
 
@@ -64,4 +81,5 @@ s.reducefn = reducefn
 
 results = s.run_server(password="changeme")
 
-print results
+# print preety
+pprint.pprint(results)
